8430982f9672e9c691a62f44f352b2efa4dcab38
HichemBenChaaben <napsterisamm@hotmail.fr>
2015-07-26 04:09:13 +0400

    Added a generic angular factory

--- packages/custom/comments/public/services/comments.js
+++ packages/custom/comments/public/services/comments.js
@@ -1,9 +1,15 @@
-'use strict';
-
-angular.module('mean.comments').factory('Comments', [
-  function() {
-    return {
-      name: 'comments'
-    };
-  }
-]);
+(function(window) {
+    'use strict';
+    angular.module('mean.comments')
+        .factory('Comments', ['$resource',
+            function($resource) {
+                return $resource('api/comments/:articleId', {
+                    articleId: '@articleId'
+                }, {
+                    update: {
+                        method: 'PUT'
+                    }
+                });
+            }
+        ]);
+}(window));
